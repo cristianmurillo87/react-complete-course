363fe56ab2077d78af3cc14aa9ad26387c191a5a
Cristian  Murillo <cam_725@hotmail.com>
2025-05-04 15:44:26 +0200

    implemented basic dynamic values

--- react-complete-course/src/App.jsx
+++ react-complete-course/src/App.jsx
@@ -1,14 +1,28 @@
+const reactDescriptions = ["Fundamental", "Crucial", "Core", "Important"];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const index = getRandomInt(reactDescriptions.length);
+  const description = reactDescriptions[index];
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
