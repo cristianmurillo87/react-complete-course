37832f16914a2ea3822e0dfb9b75e7ffda9e5ca9
Cristian  Murillo <cam_725@hotmail.com>
2025-05-07 22:42:11 +0200

    implemented displaying conditional components

--- react-complete-course/src/App.jsx
+++ react-complete-course/src/App.jsx
@@ -1,11 +1,11 @@
 import Header from "./components/header/header";
 import CoreConcept from "./components/core-concept/core-concept";
 import TabButton from "./components/tab-button";
-import { CORE_CONCEPTS } from "./data";
+import { CORE_CONCEPTS, EXAMPLES } from "./data";
 import { useState } from "react";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("Please select a topic");
+  const [selectedTopic, setSelectedTopic] = useState();
 
   const handleSelect = (topic) => {
     setSelectedTopic(topic);
@@ -48,8 +48,19 @@ function App() {
               State
             </TabButton>
           </menu>
+
+          {!selectedTopic ? (
+            <p>Please select a topic.</p>
+          ) : (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )}
         </section>
-        {selectedTopic}
       </main>
     </div>
   );
